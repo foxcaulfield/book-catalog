10048af47a991455029066de778728b0e2e47f83
Fox Caulfield <fox.caulfield@gmail.com>
2023-10-23 19:58:56 +0300

    Add Catalog methods

--- headers/Catalog.hpp
+++ headers/Catalog.hpp
@@ -1,13 +1,15 @@
 #ifndef CATALOG_H
 #define CATALOG_H
 
-#include "globals.hpp"
 #include "Book.hpp"
+#include "globals.hpp"
 
 class Catalog {
    private:
    public:
     std::vector<Book> books;
+    void add_book(Book book);
+    size_t get_books_count();
 };
 
 #endif
--- src/Catalog.cpp
+++ src/Catalog.cpp
@@ -1,1 +1,9 @@
 #include "Catalog.hpp"
+
+void Catalog::add_book(Book book) {
+    books.push_back(book);
+}
+
+size_t Catalog::get_books_count() {
+    return books.size();
+}
